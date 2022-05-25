6345ef79ef9f83a41909b5d5662992ac3201bd4f
Marshnme <jildsmiles@yahoo.com>
2022-05-25 15:21:12 -0400

    added gameBoard module and displaying board on page with function in the gameBoard module.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-// alert("wassup")
+
+// This is a module 
+const gameBoard = (() => {
+    const gameBoardContainer = document.querySelector(".game-board")
+    let board = ["O","X","X",
+                 "X","O","X",
+                 "O","X","O"];
+
+    let displayBoard = function(){
+        for(let i = 0; i<board.length; i++){
+            let gameTile = document.createElement("div")
+            gameTile.textContent = board[i];
+            gameTile.classList.add("game-tile",`game-tile-${i}`)
+            gameBoardContainer.appendChild(gameTile)
+        }
+    }
+
+    return{
+        displayBoard,
+    }
+})();
+
+gameBoard.displayBoard()
